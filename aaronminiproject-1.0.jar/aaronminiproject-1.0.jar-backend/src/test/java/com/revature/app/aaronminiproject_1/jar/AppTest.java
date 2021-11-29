76cd8cb222e75fa087faad553ddb14f87c1b64cd
AarSchoSkIg <aaronschomer@yahoo.com>
2021-11-29 18:06:01 -0500

    On Nov 29, 2021 at 5:55pm: Created a maven project called aaronminiproject-1.0.jar and created the pom.xml file

---
+++ aaronminiproject-1.0.jar/aaronminiproject-1.0.jar-backend/src/test/java/com/revature/app/aaronminiproject_1/jar/AppTest.java
@@ -0,0 +1,12 @@
+package com.revature.app.aaronminiproject_1.jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
