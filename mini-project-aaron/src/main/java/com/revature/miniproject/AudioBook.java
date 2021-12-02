090157a948ee56270c3dabd11252bea63d345b8c
AarSchoSkIg <aaronschomer@yahoo.com>
2021-12-02 17:55:44 -0500

    On 12/02/2021 at 5:30pm: changed name of wishlistedAudioBook to eachAudioBook, as well as initialized and declared a static list for each AudioBook object in Class AudioBook. added an add method to audiobooks linked list that adds each audioBook object to list. At 5:35pm edited ctx.result to display the parameters of each Audiobook in a clearer manner. Also added comments to the for each loop in BookDriver class and the nestd if loop explaining what those methods do. At 5:45pm fixed spelling of author in HTML from a uppercase A to a lowercase a. The program runs as expected after the additions. Finished froject at 5:50pm on december02 2021.

--- mini-project-aaron/src/main/java/com/revature/miniproject/AudioBook.java
+++ mini-project-aaron/src/main/java/com/revature/miniproject/AudioBook.java
@@ -1,9 +1,13 @@
 package com.revature.miniproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AudioBook {
-	String genre;
 	String title;
+	String genre;
 	String author;
 	String narrator; //field for the speaker of the audiobook
+	static List<AudioBook> audiobooks = new ArrayList<AudioBook>();
 
 }
--- mini-project-aaron/src/main/java/com/revature/miniproject/BookDriver.java
+++ mini-project-aaron/src/main/java/com/revature/miniproject/BookDriver.java
@@ -1,13 +1,14 @@
 package com.revature.miniproject;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import io.javalin.Javalin;
 
 public class BookDriver {
 	
-	 static List<AudioBook> audiobooks = new ArrayList<AudioBook>();
+	static List<AudioBook> audiobooks = new LinkedList<AudioBook>();
+	 
 	
 	public static void main(String[] args) {
 		
@@ -16,18 +17,20 @@ public class BookDriver {
 		app.start(4444); //Javalin app is started in port 4444
 		
 		app.post("/audiobooks", ctx -> {
-			
+			System.out.println(ctx.formParamMap());
 			AudioBook audioBook = new AudioBook();
 			audioBook.title = ctx.formParam("title");
 			audioBook.genre = ctx.formParam("genre");
 			audioBook.author = ctx.formParam("author");
 			audioBook.narrator = ctx.formParam("narrator");
 			
+			audiobooks.add(audioBook);
+			
 			String enteredAudioBookPlusDescription = "";
 			
-			for(AudioBook wishlistedAudioBook : audiobooks) {
-				if(wishlistedAudioBook  != null) {
-					enteredAudioBookPlusDescription += wishlistedAudioBook.title + wishlistedAudioBook.genre + wishlistedAudioBook.author + wishlistedAudioBook.narrator + "<br>";
+			for(AudioBook eachAudioBook : audiobooks) { //trying to create a for each loop to iterate through each new audiobook added by the user to the linked list and show the list under the HTML form
+				if(eachAudioBook  != null) { //setting up a restriction for the text box, to exclude null being able to be printed
+					ctx.result(enteredAudioBookPlusDescription += "AudioBook title: " + eachAudioBook.title + ", " + "  Genre: " + eachAudioBook.genre + ", " + "  Author: " + eachAudioBook.author + ", " + "  Narrator: " + eachAudioBook.narrator + "." + "<br>");
 				}
 			}
 			
